72b8de39a3d1282efa4d1538b608a219fa2f7cea
ikhsan laisa <ikhsanlaisa96@gmail.com>
2018-03-25 18:11:33 +0700

    fix

---
+++ app/src/main/java/com/example/ikhsanlaisa/ikhsan_1202150084_studycase5/SwipeHelper.java
@@ -0,0 +1,31 @@
+package com.example.ikhsanlaisa.ikhsan_1202150084_studycase5;
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+/**
+ * Created by ikhsanlaisa on 25/03/18.
+ */
+
+public class SwipeHelper extends ItemTouchHelper.SimpleCallback {
+    RecyclerAdapter adapter;
+
+    public SwipeHelper(int dragDirs, int swipeDirs){
+        super(dragDirs, swipeDirs);
+    }
+
+    public SwipeHelper(RecyclerAdapter adapter){
+        super(ItemTouchHelper.UP | ItemTouchHelper.DOWN, ItemTouchHelper.LEFT);
+        this.adapter = adapter;
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+        adapter.dismissData(viewHolder.getAdapterPosition());
+    }
+}
